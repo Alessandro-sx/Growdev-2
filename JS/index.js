60b679e30de1986ca44de28028505138995c6548
Alessandro Xavier <alessandro_sx@outlook.com>
2021-01-27 21:35:14 -0300

    Primeiro commit

---
+++ JS/index.js
@@ -0,0 +1,31 @@
+var nome = "Alessandro Xavier";
+var cargo = "Estudante de Análise e Desenvolvimento de Sistemas";
+
+var nomeHtml = document.getElementById("nome-no-html");
+var cargoHtml = document.getElementById("cargo-no-html");
+var texto1 = document.getElementById("texto-1");
+var texto2 = document.getElementById("texto-2");
+
+function colocarNomeNoHtml(nome) {
+    nomeHtml.innerHTML = nome;
+}
+
+function colocarCargoNoHtml(cargo) {
+    cargoHtml.innerHTML = cargo;
+}
+
+function clickNoProjetos(){
+    console.log("clicou no botão projetos");
+    texto2.style.display ="block";
+    texto1.style.display ="none";
+}
+
+function  clickNoSobre(){
+    console.log("ckicou no botão Sobre");
+    texto1.style.display ="block";
+    texto2.style.display ="none";
+}
+
+
+colocarNomeNoHtml(nome);
+colocarCargoNoHtml(cargo);

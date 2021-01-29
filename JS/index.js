8651f22413d25e15b46db065f8b1f27bde1b2384
Alessandro Xavier <alessandro_sx@outlook.com>
2021-01-28 22:33:23 -0300

    Atualização do meu curriculo

--- JS/index.js
+++ JS/index.js
@@ -5,6 +5,7 @@ var nomeHtml = document.getElementById("nome-no-html");
 var cargoHtml = document.getElementById("cargo-no-html");
 var texto1 = document.getElementById("texto-1");
 var texto2 = document.getElementById("texto-2");
+var texto3 = document.getElementById("texto-3");
 
 function colocarNomeNoHtml(nome) {
     nomeHtml.innerHTML = nome;
@@ -14,15 +15,24 @@ function colocarCargoNoHtml(cargo) {
     cargoHtml.innerHTML = cargo;
 }
 
-function clickNoProjetos(){
-    console.log("clicou no botão projetos");
+function clickNoExperiências(){
+    console.log("clicou no botão Experiências");
     texto2.style.display ="block";
     texto1.style.display ="none";
+    texto3.style.display ="none";
 }
 
 function  clickNoSobre(){
-    console.log("ckicou no botão Sobre");
+    console.log("clicou no botão Sobre");
     texto1.style.display ="block";
+    texto2.style.display ="none";
+    texto3.style.display ="none";
+    
+}
+function  clickNoCursos(){
+    console.log("clicou no botão cursos");
+    texto3.style.display ="block";
+    texto1.style.display ="none";
     texto2.style.display ="none";
 }
 
